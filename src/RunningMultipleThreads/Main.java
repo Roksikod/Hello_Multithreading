f8b729d1122175fd7e7cbea5422388502b7da9e3
Roksana Zalesskaya <Ksroksana@bk.ru>
2024-01-10 23:29:08 +0300

    Initial commit

---
+++ src/RunningMultipleThreads/Main.java
@@ -0,0 +1,24 @@
+package RunningMultipleThreads;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Start");
+// Runnable myRunnable = new MyRunnable();
+// Thread thread = new Thread(myRunnable);
+// Thread thread = new MyThread();
+        //или, что то же самое по сути, но меньше кода - используется анонимный класс
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1000; i++) {
+                    System.out.print(i);
+                }
+            }
+        });
+        thread.start();
+        for (int i = 0; i < 1000; i++) {
+            System.out.print("M");
+        }
+        System.out.println("\nFinish");
+    }
+}

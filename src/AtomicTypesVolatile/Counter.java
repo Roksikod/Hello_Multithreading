e094fe330f698aa26a956d6d8bffff8e839905fe
Roksana Zalesskaya <Ksroksana@bk.ru>
2024-01-12 13:04:26 +0300

    synchronized or join

---
+++ src/AtomicTypesVolatile/Counter.java
@@ -0,0 +1,19 @@
+package AtomicTypesVolatile;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Counter {
+    private AtomicInteger value = new AtomicInteger();
+
+    public void inc() {
+        value.getAndIncrement();
+    }
+
+    public void dec() {
+        value.getAndDecrement();
+    }
+
+    public int getValue() {
+        return value.intValue();
+    }
+}

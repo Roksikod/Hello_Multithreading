50ac9eb02121870a15463a64800bce3415aa9a8c
Roksana Zalesskaya <Ksroksana@bk.ru>
2024-01-12 14:14:52 +0300

    CountDownLatch and ExecutorService

---
+++ src/CountDownLatchExecutorService/Main.java
@@ -0,0 +1,35 @@
+package CountDownLatchExecutorService;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            final  int index = i;
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("Start - " + index);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println("Finish - " + index);
+                }
+                });
+            threads.add(thread);
+            thread.start();
+        }
+        for (Thread thread:threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println("All threads are terminated");
+    }
+}

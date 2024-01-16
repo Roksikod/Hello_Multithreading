104b09315394e2451a216e919f172afcc29ddccd
Roksana Zalesskaya <Ksroksana@bk.ru>
2024-01-16 14:45:55 +0300

    ThreadFactory, Callable and Future

--- src/CountDownLatchExecutorService/Main1.java
+++ src/CountDownLatchExecutorService/Main1.java
@@ -6,11 +6,12 @@ import java.util.concurrent.Executors;
 
 public class Main1 {
     public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(5);
-        CountDownLatch countDownLatch = new CountDownLatch(10);
+        ExecutorService executorService = Executors.newFixedThreadPool(5); //фиксированный пул потоков
+        //рационально использовать, если много разных задач в отдельных потоках
+        CountDownLatch countDownLatch = new CountDownLatch(10); //счетчик для десяти потоков
         for (int i = 0; i < 10; i++) {
             final int index = i;
-            executorService.execute(new Runnable() {
+            executorService.execute(new Runnable() { //передаем потоки на исполнение по мере освобождения потоков
                 @Override
                 public void run() {
                     System.out.println("Start - " + index);
@@ -25,7 +26,7 @@ public class Main1 {
             });
         }
         try {
-            countDownLatch.await();
+            countDownLatch.await(); //пока счетчик не станет ноль, ждем
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
